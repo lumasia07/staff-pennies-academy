6c5677c752933ea277cbbf6680ca191e35461301
Lumasia07 <lumasiastancelaus@gmail.com>
2026-06-10 21:52:22 +0300

    fix

---
+++ public/build/assets/ApplicationLogo-BlmS03aN.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-DlGVnXj2.js";function n(o){return t.jsx("img",{...o,src:"/images/dtn-logo.png",alt:"Logo"})}export{n as A};
--- public/build/assets/ApplicationLogo-vy9Mj-Ot.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-B9lE8855.js";function n(o){return t.jsx("img",{...o,src:"/images/dtn-logo.png",alt:"Logo"})}export{n as A};
